16effbfe6dc9e0f97dc8cc15bf1912146fe0db8c
suryaprakashclarisco <suryaprakash@clariscosolutions.com>
2026-02-24 20:44:15 +0530

    Change in PatientModel

--- Backend_/src/models/patientModel.js
+++ Backend_/src/models/patientModel.js
@@ -4,55 +4,44 @@ const patientSchema = new mongoose.Schema(
   {
     userId: {
       type: mongoose.Schema.Types.ObjectId,
-      ref: "User",   // Must match your User model name
+      ref: "User",
       required: true,
     },
-
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
     age: {
       type: Number,
       required: true,
     },
-
     gender: {
       type: String,
       enum: ["male", "female", "other"],
       required: true,
     },
-
     bloodGroup: {
       type: String,
       enum: ["A+", "A-", "B+", "B-", "AB+", "AB-", "O+", "O-"],
       required: true,
     },
-
     allergies: {
-      type: [String],   // Array of strings
+      type: [String],
       default: [],
     },
-
     medicalHistory: {
-      type: String,
-      default: "",
+      type: [String], 
+      default: [],
     },
-
     emergencyContact: {
-      name: {
-        type: String,
-        required: true,
-      },
-      relation: {
-        type: String,
-        required: true,
-      },
-      phone: {
-        type: String,
-        required: true,
-      },
+      name: { type: String, required: true },
+      relation: { type: String, required: true },
+      phone: { type: String, required: true },
     },
   },
-       { timestamps: true }
+  { timestamps: true }
 );
 
 const Patient = mongoose.model("Patient", patientSchema);
-
 export default Patient;
